9b22c933beb736421ecf80b513951a906d4f810f
114186_RameNicolas <128998615+114186-RameNicolas@users.noreply.github.com>
2023-08-03 17:53:19 -0300

    practica

--- PracticaBienDesde0/CodeFirst_Empleados/API_CodeFirst_Empleado/Data/ApplicationContext.cs
+++ PracticaBienDesde0/CodeFirst_Empleados/API_CodeFirst_Empleado/Data/ApplicationContext.cs
@@ -1,8 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API_CodeFirst_Empleado.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace API_CodeFirst_Empleado.Data
 {
     public class ApplicationContext : DbContext
     {
+        public ApplicationContext()
+        {
+
+        }
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Cargo> Cargo { get; set; }
+        public virtual DbSet<Ciudad> Ciudad { get; set; }
+        public virtual DbSet<Empleado> Empleado { get; set; }
+        public virtual DbSet<Sucursal> Sucursal { get; set; }
     }
 }
--- PracticaBienDesde0/CodeFirst_Empleados/API_CodeFirst_Empleado/Program.cs
+++ PracticaBienDesde0/CodeFirst_Empleados/API_CodeFirst_Empleado/Program.cs
@@ -1,3 +1,7 @@
+using API_CodeFirst_Empleado.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ConexionBaseDatos"));
+});
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+});
+
+builder.Services.AddCors(config =>
+{
+    config.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
